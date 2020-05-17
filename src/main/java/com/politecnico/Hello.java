567a9276f7b715bbff259abbc76006705c82ddff
raulgp19 <55979635+raulgp19@users.noreply.github.com>
2020-05-17 16:53:13 +0200

    1.0

---
+++ src/main/java/com/politecnico/Hello.java
@@ -0,0 +1,8 @@
+package com.politecnico;
+
+public class Hello {
+    public static void main(String[] args) {
+       Sumar sumar = new Sumar();
+        System.out.println(sumar.Sumar(1,2));
+    }
+}
---
+++ src/main/java/com/politecnico/Sumar.java
@@ -0,0 +1,9 @@
+package com.politecnico;
+
+public class Sumar {
+
+    public int Sumar(int a, int b)
+    {
+        return a + b;
+    }
+}
